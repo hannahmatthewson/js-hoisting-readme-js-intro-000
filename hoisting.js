02cea813ce57fa1db3fdc682a678fead9d3b81c3
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-07 12:25:29 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
@@ -19,5 +19,3 @@ function sayMyName() {
     var name = "Kristin";
     console.log(name);
   }
-
-  sayMyName()();
